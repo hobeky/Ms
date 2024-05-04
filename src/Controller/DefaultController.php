58281599d65603d60a2e75f442ee36a125ff3ae1
hobeky <hobeky@gmail.com>
2024-05-04 11:17:30 +0200

    changed paths, added correct active class for current page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,7 +6,6 @@ use App\Entity\Teacher;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController
@@ -15,24 +14,88 @@ class DefaultController extends AbstractController
         private readonly EntityManagerInterface $entityManager,
     )
     {
-
     }
-    #[Route('/default/{templateName}', name: 'app_default')]
-    public function index(?string $templateName = null): Response
-    {
-        if (is_null($templateName)){
-            $templateName = 'about';
-        }
 
+    #[Route('/o-nas', name: 'about_us')]
+    public function aboutUs(): Response
+    {
+        return $this->render('template/about.html.twig', [
+            'heroName' => 'about us'
+        ]);
+    }
+
+    #[Route('/recenzie', name: 'reviews')]
+    public function reviews(): Response
+    {
+        return $this->render('template/reviews.html.twig', [
+            'heroName' => 'about us'
+        ]);
+    }
+
+    #[Route('/kontakt', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('template/contact.html.twig', [
+            'heroName' => 'about us'
+        ]);
+    }
+
+    #[Route('/pre-rodicov', name: 'for_parents')]
+    public function forParents(): Response
+    {
+        return $this->render('template/for-parents.html.twig', [
+            'heroName' => 'about us'
+        ]);
+    }
+
+    #[Route('/nas-tim', name: 'teachers')]
+    public function ourTeam(): Response
+    {
         $teacherRepo = $this->entityManager->getRepository(Teacher::class);
         $teachers = $teacherRepo->findAll();
-        $templatePath = 'template/' . $templateName . '.html.twig';
 
-
-        return  $this->render($templatePath, [
-            'heroName' => $templateName,
+        return $this->render('template/teachers.html.twig', [
+            'heroName' => 'about us',
             'teachers' => $teachers
         ]);
+    }
 
+    #[Route('/galeria', name: 'gallery')]
+    public function gallery(): Response
+    {
+        return $this->render('template/gallery.html.twig', [
+            'heroName' => 'about us'
+        ]);
+    }
+
+    #[Route('/jedalny-listok', name: 'long_menu')]
+    public function menu(): Response
+    {
+        return $this->render('template/meal-menu.html.twig', [
+            'heroName' => 'about us'
+        ]);
+    }
+
+    #[Route('/jedalny-listok-kratky', name: 'short_menu')]
+    public function shortMenu(): Response
+    {
+        return $this->render('template/menu-short.html.twig', [
+            'heroName' => 'about us'
+        ]);
+    }
+
+    #[Route('/rozvrh', name: 'schedule')]
+    public function schedule(): Response
+    {
+        return $this->render('template/time-table.html.twig', [
+            'heroName' => 'about us'
+        ]);
+    }
+    #[Route('/bezpecnost-deti', name: 'child_safety')]
+    public function childSafety(): Response
+    {
+        return $this->render('template/kid-security.html.twig', [
+            'heroName' => 'about us'
+        ]);
     }
 }
