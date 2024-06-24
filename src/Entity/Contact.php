3c4d7ef2ca4d8fb09ad0b30e27bd1ca470ecd469
hobeky <hobeky@gmail.com>
2024-06-24 20:03:58 +0200

    added contact as entity plus added new migration

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -2,33 +2,48 @@
 
 namespace App\Entity;
 
-use AllowDynamicProperties;
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
-
-#[AllowDynamicProperties]
+#[ORM\Entity]
+#[ORM\Table(name: 'contact')]
 class Contact
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
     #[Assert\NotBlank(message: 'Name is required.')]
     #[Assert\Length(max: 255)]
     private string $name;
 
+    #[ORM\Column(type: 'string', length: 255)]
     #[Assert\NotBlank(message: 'Email is required.')]
     #[Assert\Email(message: 'The email {{ value }} is not a valid email.')]
     private string $email;
 
+    #[ORM\Column(type: 'string', length: 15)]
     #[Assert\NotBlank(message: 'Phone number is required.')]
     #[Assert\Length(max: 15)]
     private string $phone;
 
+    #[ORM\Column(type: 'string', length: 255)]
     #[Assert\NotBlank(message: 'Topic is required.')]
     #[Assert\Length(max: 255)]
     private string $topic;
 
+    #[ORM\Column(type: 'text')]
     #[Assert\NotBlank(message: 'Message is required.')]
     private string $message;
 
     // Getters and Setters
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
     public function getName(): string
     {
         return $this->name;
